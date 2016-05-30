0076a11c9a704346f7ffb445352f16b73e044bd0
prashanth ramesh <prashanth ramesh>
2016-05-30 16:29:11 -0400

    second deliverable

--- app/src/main/java/com/example/manu/tic_tac_toe/StartScreen.java
+++ app/src/main/java/com/example/manu/tic_tac_toe/StartScreen.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
@@ -14,14 +16,17 @@ public class StartScreen extends AppCompatActivity {
 
     Button btn_new_game, btn_exit, btn_help;
     PopupWindow popUp;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_start_screen);
         btn_new_game = (Button) findViewById(R.id.btn_newgame);
         btn_exit = (Button) findViewById(R.id.btn_exitgame);
         btn_help = (Button) findViewById(R.id.btn_help);
+        linearLayout = (LinearLayout)findViewById(R.id.start_screen_layout);
         popUp = new PopupWindow(this);
 
         btn_new_game.setOnClickListener(new View.OnClickListener() {
@@ -40,18 +45,16 @@ public class StartScreen extends AppCompatActivity {
         btn_help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Help_text_message="\"Tic-tac-toe (also known as Noughts and crosses or Xs and Os) is a  game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.\\n\" +\n" +
-                        "            \"\n\" +\n" +
-                        "            \"The following example game is won by the first player, X:\\n\" +\n" +
-                        "            \"\\n\" +\n" +
-                        "            \"\\n\" +\n" +
-                        "            \"\\n\" +\n" +
-                        "            \"Game of Tic-tac-toe, won by X\\n\" +\n" +
-                        "            \"\\n\" +\n" +
-                        "            \"The winner will get a gift.\\n\"";
+                String Help_text_message="Tic-tac-toe (also known as Noughts and crosses or Xs and Os) is a  game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.\n" +
+                        "\n" +
+                        "The following example game is won by the first player, X:\n" +
+                        "\n" +
+                        "\n" +
+                        "Game of Tic-tac-toe, won by X\n" +
+                        "\n" +
+                        "The winner will get a gift."+
+                        "\n";
                 showHelp(Help_text_message, 0);
-
-
 
 
 
